0fe5a8ea8d52735d886c71d83c2dc7904f56acd5
Vladimir_Melentev <kelev_ra63@icloud.com>
2024-01-28 16:37:49 +0300

    Add CS diagram

--- end_with_cs.rb
+++ end_with_cs.rb
@@ -1,11 +1,16 @@
 def word_end_cs
-  puts "Введите слово: "
-  word = gets.chomp.to_s
+  loop do
+    puts "Введите слово: "
+    word = gets.chomp.to_s
 
-  if word.upcase.end_with?("CS")
-    puts 2**word.length
-  else
-    puts word.reverse
+
+    if word.upcase.end_with?("CS")
+      puts 2**word.length
+    else
+      puts word.reverse
+    end
+
+    break if word.downcase == "exit"
   end
 end
 
