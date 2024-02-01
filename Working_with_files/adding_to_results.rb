8e5cc0c77612313fc7007253240c95c7ac130981
Vladimir_Melentev <kelev_ra63@icloud.com>
2024-02-01 14:58:36 +0300

    Added file results

--- Working_with_files/adding_to_results.rb
+++ Working_with_files/adding_to_results.rb
@@ -5,4 +5,25 @@ def index
   File.foreach(FILE_PATH) { |element| puts element}
 end
 
-index
+while true
+puts "Enter the age of students to add: "
+age_students = gets.chomp.to_s
+
+if age_students == "-1" || File.size(FILE_PATH) == File.size(RESULTS)
+  break
+end
+
+file = File.open(RESULTS, "a")
+File.foreach(FILE_PATH){ |student| file.puts(student) if student.include?(age_students) }
+
+file.close
+end
+
+
+
+
+
+
+
+
+
